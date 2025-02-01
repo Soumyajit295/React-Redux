82548330af02b5b98f55a9d4042fd2b1c60f3c4a
Soumyajit de <soumyajit.de20190@gmail.com>
2025-02-01 19:03:11 +0530

    Configure RTK in project

--- appwrite-blogApp/src/App.jsx
+++ appwrite-blogApp/src/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './Appwrite/auth'
 import './App.css'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './Components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <h1>Chai aur Appwrite</h1>
-    </>
-  )
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
